d716827eab4f3af81a708daa1a5b7291bc5aed99
Lucy Ruiz <lucyruizwa@gmail.com>
2024-03-10 21:53:49 -0700

    function to get winner, completed

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
 //DECLARE VARIABLES
 let turn
 let grid
+const winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]]
 
 //FUNCTIONS
 //Declare init
 function init() {
     turn = "O"
     grid = [null, null, null, null, null, null, null, null, null]
+}
+
+// Alternate players
+function switchPlayer() {
+    if (turn === "O") {
+        turn = "X"
+    } else {
+        turn = "O"
+    }
+}
+
+// Victory conditions
+function victory() {
+    winningCombinations.forEach(function(oneCombination){
+        let elA = grid[oneCombination[0]];
+        let elB = grid[oneCombination[1]];
+        let elC = grid[oneCombination[2]];
+        if (elA === "O" && elB === "O" && elC === "O") {
+            return "O";
+        }
+        if (elA === "X" && elB === "X" && elC === "X") {
+            return "X";
+        } 
+    })
+    return null
 }
 
 // Create a function for evt. Include preventDefault()
@@ -18,12 +44,20 @@ function playerClick(evt) {
     let i = parseInt(evt.target.id, 10)
     console.log("there was a click")
     console.log(evt.target.id)
-// Update grid array with player turn
-    grid[i] = turn
+    // Verify this element has not been clicked
+    if (grid[i] !== null) {
+        return 
+    } else {
+    // Update grid array with player turn
+        grid[i] = turn
+    }
     console.log(grid)
+    switchPlayer()
+    let winner = victory()
 }
 
 init()
+
 //EVENTS
 // Declare handleClick
 document.getElementById("grid").addEventListener("click", playerClick);
